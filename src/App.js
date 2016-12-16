73be766d2f6c4433bc7afc0c1eae2f87aa7cfac5
Austin Hansen <austintatious@gmail.com>
2016-12-15 21:24:13 -0800

    inputs for water, oil, and super fat

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label> Weight
+          <input type="number" name="weight"  placeholder="grams" />
+        </label>
+        <label> Water Percentage
+          <input type="number" name="water-percentage" />
+        </label>
+        <label> Super Fat %
+          <input type="number" name="fat" defaultValue={5} />
+        </label>
       </div>
     );
   }
